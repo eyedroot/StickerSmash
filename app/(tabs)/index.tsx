abb456ace2f41d1a19d2cdc79fcad5eeb5a79fa8
byzz <eyedroot@gmail.com>
2024-10-06 15:50:33 +0900

    Chapter 7: Take a screenshot (react-native-view-shot, expo-media-library)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,21 +2,25 @@ import {StyleSheet, View} from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
-import {useState} from "react";
+import {useRef, useState} from "react";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
 import EmojiSticker from "@/components/EmojiSticker";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
+import * as MediaLibrary from 'expo-media-library';
+import {captureRef} from 'react-native-view-shot';
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function Index() {
+  const imageRef = useRef(null);
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [pickedEmoji, setPickedEmoji] = useState<string | undefined>(undefined);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -45,12 +49,35 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+
+      if (localUri) {
+        alert('Image saved to gallery.');
+      }
+    } catch (e) {
+      console.error(e);
+    }
   };
+
+  // When the app loads for the first time and the permission status is neither granted nor denied,
+  // the value of the `status` is `null`.
+  if (status === null) {
+    requestPermission();
+  }
 
   return (
     <GestureHandlerRootView style={styles.container}>
-      <View style={styles.imageContainer}>
+      <View
+        ref={imageRef}
+        style={styles.imageContainer}
+        collapsable={false}
+      >
         <ImageViewer source={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
