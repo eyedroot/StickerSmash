08d90a9e4539aa670ca5577eff506e1ce5573ae2
byzz <eyedroot@gmail.com>
2024-10-06 16:03:24 +0900

    Chapter 8: Handle platform differences (useRef, Platform, types.d.ts)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, View} from "react-native";
+import {Platform, StyleSheet, View} from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
@@ -11,11 +11,21 @@ import EmojiSticker from "@/components/EmojiSticker";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
 import * as MediaLibrary from 'expo-media-library';
 import {captureRef} from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function Index() {
+  // 변경 가능한 값(mutable value)을 저장하고, 컴포넌트의 생명주기 전체에 걸쳐 '동일한' 참조를 유지합니다.
+  // 값이 변경되어도 컴포넌트는 재렌더링되지 않습니다.
+  // DOM 요소나 자식 컴포넌트에 직접 접근하거나, 재렌더링 사이에서도 값이 유지되어야 할 때 사용합니다.
+  // 타이머 ID, 이전 상태 값, 외부 라이브러리 인스턴스 등을 저장하는데 유용합니다.
+  // 반면에 useState는 상태 변경 시 컴포넌트를 재렌더링합니다.
+  // useRef는 값이 변경되어도 재렌더링을 트리거하지 않습니다.
+  // 사용 목적: `useState` UI에 영향을 미치는 상태를 관리합니다.
+  // 사용 목적: `useRef` 재렌더링 없이 값을 저장하거나, DOM 요소에 직접 접근합니다.
   const imageRef = useRef(null);
+
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
@@ -49,21 +59,38 @@ export default function Index() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
+        await MediaLibrary.saveToLibraryAsync(localUri);
 
-      if (localUri) {
-        alert('Image saved to gallery.');
+        if (localUri) {
+          alert('Image saved to gallery.');
+        }
+      } catch (e) {
+        console.error(e);
       }
-    } catch (e) {
-      console.error(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.error(e);
+      }
     }
-  };
+  }
 
   // When the app loads for the first time and the permission status is neither granted nor denied,
   // the value of the `status` is `null`.
