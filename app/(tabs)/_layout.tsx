e4ba2c00166b935178ec0fb98d0a4e6d1663615e
byzz <eyedroot@gmail.com>
2024-10-12 13:57:29 +0900

    Animation Lottie

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,49 +1,65 @@
-import {Tabs} from "expo-router";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import {MaterialIcons} from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: '#ffd33d',
+        tabBarActiveTintColor: "#ffd33d",
         headerStyle: {
-          backgroundColor: '#25292e',
+          backgroundColor: "#25292e",
         },
         headerShadowVisible: false,
-        headerTintColor: '#fff',
+        headerTintColor: "#fff",
         tabBarStyle: {
-          backgroundColor: '#25292e',
-        }
+          backgroundColor: "#25292e",
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            />
           ),
         }}
       />
       <Tabs.Screen
         name="about"
         options={{
-          title: 'About',
+          title: "About",
           tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+            <Ionicons
+              name={
+                focused ? "information-circle" : "information-circle-outline"
+              }
+              color={color}
+              size={24}
+            />
           ),
         }}
       />
       <Tabs.Screen
         name="ui"
         options={{
-          title: 'UI',
+          title: "UI",
           tabBarIcon: ({ color, focused }) => (
             <MaterialIcons name="code" color={color} size={24} />
-          )
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="lottie"
+        options={{
+          title: "Lottie",
         }}
       />
     </Tabs>
-  )
+  );
 }
---
+++ app/(tabs)/lottie.tsx
@@ -0,0 +1,43 @@
+import LottieView from "lottie-react-native";
+import { useRef } from "react";
+import { Button, StyleSheet, View } from "react-native";
+
+export default function Lottie() {
+  const animation = useRef<LottieView>(null);
+
+  return (
+    <View style={styles.animationContainer}>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "#eee",
+        }}
+        source={require("@/assets/lotties/animation.json")}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Restart Animation"
+          onPress={() => {
+            animation.current?.reset();
+            animation.current?.play();
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+});
